fad9a6f582f4908efd20f0a07650411ae7989c06
maffettone <phillip.maffettone@gmail.com>
2022-07-09 13:32:49 -0400

    Everything as LIST

--- mmm_experiments/agents/base.py
+++ mmm_experiments/agents/base.py
@@ -410,7 +410,7 @@ class DrowsyAgent(Agent, ABC):
     def ask(self, batch_size: int) -> Tuple[dict, Sequence]:
         self.counter += 1
         logging.debug(f"Counter={self.counter}")
-        return dict(batch_size=batch_size), [0.0]
+        return dict(batch_size=[batch_size]), [0.0]
 
     def report(self):
         pass
