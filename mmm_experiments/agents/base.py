45779e18a49f1aeb1967f1f6926cea20febeba41
maffettone <phillip.maffettone@gmail.com>
2022-07-10 15:41:16 -0400

    list iteration error

--- mmm_experiments/agents/base.py
+++ mmm_experiments/agents/base.py
@@ -402,7 +402,7 @@ class GeometricResolutionMixin(SequentialAgentMixin):
             self.acumulated_stops = 0
             self.independent_cache = sorted(self.independent_cache)
             points = []
-            for i in range(len(self.independent_cache)):
+            for i in range(len(self.independent_cache) - 1):
                 points.append((self.independent_cache[i + 1] - self.independent_cache[i]) / 2)
             self.points_per_batch = len(points)
             doc = dict(ask_ready=[True], size_of_batch=[len(points)], proposal=[points])
