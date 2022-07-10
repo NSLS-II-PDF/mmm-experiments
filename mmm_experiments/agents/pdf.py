bf1568969c2272f01d14b8e405a7e658b68d16fe
maffettone <phillip.maffettone@gmail.com>
2022-07-10 17:22:45 -0400

    remove quick plan

--- mmm_experiments/agents/pdf.py
+++ mmm_experiments/agents/pdf.py
@@ -79,7 +79,7 @@ class PDFAgent(Agent, ABC):
 
     def measurement_plan_args(self, x_position) -> list:
         """Plan to be writen than moves to an x_position then jogs up and down relatively in y"""
-        return ["Grid_X", x_position + self.measurement_origin, 5]  # TODO to 30
+        return ["Grid_X", x_position + self.measurement_origin, 30]
 
     def measurement_plan_kwargs(self, point) -> dict:
         return {"sample_number": 15}
