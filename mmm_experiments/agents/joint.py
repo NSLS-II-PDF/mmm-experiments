5b00a245ec1427dc257dd9066cf20d66ecea932e
maffettone <phillip.maffettone@gmail.com>
2022-07-10 12:12:53 -0400

    Sequential agent for both

--- mmm_experiments/agents/joint.py
+++ mmm_experiments/agents/joint.py
@@ -97,3 +97,53 @@ class MonarchBMMSubjectPDF(GeometricResolutionMixin, MonarchSubjectBase, BMMAgen
                 r = self.subject_manager.item_add(plan, pos="front")
                 logging.info(f"Sent subject http-server request for point {point}\n." f"Received reponse: {r}")
             doc["subject_points"] = [subject_points]
+
+
+class SequentialMonarchPDF(SequentialAgentMixin, MonarchSubjectBase, PDFAgent):
+    """"""
+
+    subject_host = BMMAgent.server_host
+    subject_api_key = BMMAgent.api_key
+    subject_plan_name = BMMAgent.measurement_plan_name
+    # FOR BMM
+
+    def __init__(
+        self,
+        *,
+        Cu_origin: Tuple[float, float],
+        Ti_origin: Tuple[float, float],
+        Cu_det_position: float,
+        Ti_det_position: float,
+        bmm_bounds: Tuple[float, float],
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.sample_position_motors = ("xafs_x", "xafs_y")
+        self.Cu_origin = Cu_origin
+        self.Ti_origin = Ti_origin
+        self.Cu_det_position = Cu_det_position
+        self.Ti_det_position = Ti_det_position
+        self.bmm_bounds = bmm_bounds
+        self.bmm_cache = []
+
+    @property
+    def subject_origin(self):
+        return self.Cu_origin[0]
+
+    def subject_plan_args(self, point):
+        return BMMAgent.measurement_plan_args(self, point)
+
+    def subject_plan_kwargs(self, point) -> dict:
+        return BMMAgent.measurement_plan_kwargs(self, point)
+
+    def ask(self, batch_size: int = 1):
+        """Standard ask, Then add plans to subject
+        The base class calls `ask` during `_add_to_queue`. In this way, the subject will get points added first,
+        and the Monarch will get the points added after the ask.
+        """
+        doc, points = super().ask(batch_size)
+        for point in points:
+            plan = BPlan(self.subject_plan_name, *self.subject_plan_args(point), **self.subject_plan_kwargs(point))
+            r = self.subject_manager.item_add(plan, pos="front")
+            logging.info(f"Sent BMM http-server priority request for point {point}\n." f"Received reponse: {r}")
+        return doc, points
