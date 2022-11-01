d2900bc8733d3184870aa94e9ffa5d064f4c3e08
maffettone <phillip.maffettone@gmail.com>
2022-11-01 08:47:22 -0400

    ENH: Add agent specific kafka consumer, and plan to produce docs for agent.

--- mmm_experiments/data/kafka_consumer.py
+++ mmm_experiments/data/kafka_consumer.py
@@ -1,8 +1,16 @@
 import datetime
+import inspect
 import pprint
 import uuid
-from bluesky_kafka import RemoteDispatcher
+import warnings
+
 import nslsii
+from bluesky_kafka import BlueskyConsumer, RemoteDispatcher
+
+
+def print_message(name, doc):
+    """Convenience print"""
+    print(f"{datetime.datetime.now().isoformat()} document: {name}\n" f"contents: {pprint.pformat(doc)}\n")
 
 
 def process_kafka_messages(beamline_acronym, data_source="runengine", target=None):
@@ -19,9 +27,6 @@ def process_kafka_messages(beamline_acronym, data_source="runengine", target=Non
         Function to pass the documents from kafka to. If not specified, defaults to printing
         the documents
     """
-
-    def print_message(name, doc):
-        print(f"{datetime.datetime.now().isoformat()} document: {name}\n" f"contents: {pprint.pformat(doc)}\n")
 
     if target is None:
         target = print_message
@@ -41,3 +46,67 @@ def process_kafka_messages(beamline_acronym, data_source="runengine", target=Non
 
     kafka_dispatcher.subscribe(target)
     kafka_dispatcher.start()
+
+
+class AgentConsumer(BlueskyConsumer):
+    """Specific consumer for mmm agents. Consumes both bluesky docs and mmm docs"""
+
+    def __init__(self, *args, agent, beamline_tla, bluesky_callbacks=(), **kwargs):
+        """
+
+        Parameters
+        ----------
+        args :
+            Positional args for BlueskyConsumer.
+            topics, bootstrap_servers, group_id
+        agent : mmm_experiments.agents.base.Agent
+            Instance of agent to use for triggering agent actions
+        beamline_tla : str
+            Beamline three letter acronym, used for setup and assurance
+        bluesky_callbacks : Iterable[Callable]
+            List of callbacks to perform on documents. This list should be similar to callbacks used in
+            RE.subscribe(), and will be processed in order.
+        kwargs :
+            kwargs for BlueskyConsumer
+        """
+        super().__init__(*args, **kwargs)
+        self.tla = beamline_tla
+        self.bluesky_callbacks = bluesky_callbacks
+        self.agent = agent
+
+    def process(self, msg) -> bool:
+        name, doc = self._deserializer(msg.value())
+        continue_polling = self.process_document(msg.topic(), name, doc)
+        return continue_polling
+
+    def process_document(self, topic, name, doc):
+        keywords = topic.split(".")
+        print(topic, name, doc, keywords)
+        if self.tla not in keywords:
+            return True
+        if ("mmm" in keywords and "agents" in keywords) and name == self.agent.agent_uid:
+
+            action = doc["action"]
+            args = doc["args"]
+            kwargs = doc["kwargs"]
+            try:
+                getattr(self.agent, action)(*args, **kwargs)
+            except AttributeError as e:
+                warnings.warn(
+                    f"Unavailable action sent to agent {self.agent.agent_uid} on topic: {topic}\n" f"{e}"
+                )
+            except TypeError as e:
+                warnings.warn(
+                    f"Type error for {action} sent to agent {self.agent.agent_uid} on topic: {topic}\n"
+                    f"Are you sure your args and kwargs were appropriate?\n"
+                    f"Args received: {args}\n"
+                    f"Kwargs received: {kwargs}\n"
+                    f"Expected signature: {inspect.signature(getattr(self.agent, action))}\n"
+                    f"{e}"
+                )
+            return True
+
+        else:
+            for callback in self.bluesky_callbacks:
+                callback(name, doc)
+            return True
