a24d47e69c0816cd152d7b2399a257aae5a6df84
Thomas A Caswell <tcaswell@bnl.gov>
2022-11-02 14:29:46 -0400

    ENH: make adjudicator work
    RE(bps.mv(sb.publish, 1)) will wait for it to finish doing its job.

--- mmm_experiments/data/adjudicator.py
+++ mmm_experiments/data/adjudicator.py
@@ -1,20 +1,35 @@
-import msgpack
 import logging
-from confluent_kafka import Consumer
 
 from threading import Lock, Thread
-from collections import defaultdict
 from bluesky_kafka import BlueskyConsumer
+
+from mmm_experiments.devices.switchboard import SwitchBoardBackend
 
 logger = logging.getLogger(name="mmm.kafka")
 
 
 class LatestNews(BlueskyConsumer):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, tla="PDF", switchboard_prefix="pdf:switchboard:", **kwargs):
         super().__init__(*args, **kwargs)
         self._lock = Lock()
         self._by_agent = {}
         self._thread = None
+        self._tla = tla
+        self._sb = SwitchBoardBackend(switchboard_prefix, name="sb")
+        self._sb.publish.subscribe(self.on_process)
+
+    def on_process(self, value, **kwargs):
+        if value == 0:
+            return
+        try:
+            self._sb.adjudicate_status.set(f"Starting work on: {value}").wait()
+            self.do_work(value)
+        finally:
+            self._sb.publish.set(0).wait()
+            self._sb.adjudicate_status.set("Done").wait()
+
+    def do_work(self, value):
+        raise NotImplementedError
 
     def process_document(self, topic, name, doc):
         if name != "agent_suggestions":
@@ -39,3 +54,50 @@ class LatestNews(BlueskyConsumer):
 
         self._thread = Thread(target=self.start)
         self._thread.start()
+
+
+class LazyDemo(LatestNews):
+    def do_work(self, value):
+        import time
+
+        self._sb.adjudicate_status.set("starting work").wait()
+        time.sleep(1)
+        self._sb.adjudicate_status.set("still working").wait()
+        time.sleep(1)
+        # TODO think through how to do this mode better
+        agent = self._sb.adjudicate_mode.get()
+        by_agent = self.by_agent
+        if agent not in by_agent:
+            print("no agent found, not sure about this mode")
+        else:
+            for suggestion in by_agent[agent]["suggestions"][self._tla]:
+                print(f"I suggest {suggestion}")
+
+
+class AgentSelector(LatestNews):
+    def do_work(self, value):
+        # TODO maybe use read?
+        # TODO think through how to do this mode better
+        agent = self._sb.adjudicate_mode.get()
+        self._sb.adjudicate_status.set(f"Listening to {agent}").wait()
+
+        by_agent = self.by_agent
+        if agent not in by_agent:
+            print(f"{agent} not one we have heard of.  Know about {list(by_agent)}")
+        else:
+            for suggestion in by_agent[agent]["suggestions"][self._tla]:
+                # TODO add to queue here!
+                print(f"I suggest {suggestion}")
+
+
+if __name__ == "__main__":
+    import uuid
+    import json
+
+    judge = AgentSelector(
+        ["suggestion.box"],
+        "localhost:9092",
+        str(uuid.uuid4()),
+        deserializer=json.loads,
+    )
+    judge.start()
--- mmm_experiments/devices/switchboard.py
+++ mmm_experiments/devices/switchboard.py
@@ -8,7 +8,6 @@ class LatchSignal(EpicsSignal):
         # going low, just be done
         if value == 0:
             return base_st
-
         # going high, set up a latch
         def make_cb():
             went_high = False
@@ -23,16 +22,29 @@ class LatchSignal(EpicsSignal):
 
             return cb
 
-        st = SubscriptionStatus(self, make_cb())
+        my_cb = make_cb()
+
+        st = SubscriptionStatus(self, my_cb, timeout=timeout)
         # questionable choice to override the base
         self._status = st & base_st
         return self._status
 
 
-class SwitchBoard(Device):
-    publish = Cpt(LatchSignal, "publish_to_queue", put_complete=True)
+class SwitchBoardClient(Device):
+    # this has tricking "latching" behavior to wait for other party to work
+    publish = Cpt(LatchSignal, "publish_to_queue", put_complete=True, timeout=15)
     adjudicate_mode = Cpt(EpicsSignal, "adjudicate_mode", put_complete=True)
+    # client can not set status
     adjudicate_status = Cpt(EpicsSignalRO, "adjudicate_status")
 
 
-sb = SwitchBoard("pdf:switchboard:", name="sb")
+class SwitchBoardBackend(Device):
+    # do not use latching behavior on backend
+    publish = Cpt(EpicsSignal, "publish_to_queue", put_complete=True)
+    # backend can not set its own mode
+    adjudicate_mode = Cpt(EpicsSignalRO, "adjudicate_mode")
+    adjudicate_status = Cpt(EpicsSignal, "adjudicate_status", put_complete=True)
+
+
+if __name__ == "__main__":
+    sb = SwitchBoardClient("pdf:switchboard:", name="sb")
