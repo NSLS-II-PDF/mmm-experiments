2a1fc667ab267d880f55c7c335ba2da6a2919321
maffettone <phillip.maffettone@gmail.com>
2022-11-07 14:46:44 -0500

    Enh: Give adjudicator REManager and examples
    - Adjudicator that adds an agent's most recent suggestions to the queue depending its mode
    - Example main for how to launch

--- mmm_experiments/adjudicators/base.py
+++ mmm_experiments/adjudicators/base.py
@@ -1,11 +1,20 @@
 import logging
 from abc import ABC, abstractmethod
 from collections import deque
-from threading import Lock, Thread
+from copy import deepcopy
+from threading import Lock
 
 from bluesky_kafka import BlueskyConsumer
+from bluesky_queueserver_api import BPlan
+from bluesky_queueserver_api.http import REManagerAPI
 
+from mmm_experiments.adjudicators.msg import AdjudicatorMsg
 from mmm_experiments.devices.switchboard import SwitchBoardBackend
+
+try:
+    from nslsii import _read_bluesky_kafka_config_file
+except ImportError:
+    from nslsii.kafka_utils import _read_bluesky_kafka_config_file
 
 logger = logging.getLogger(name="mmm.adjudicators")
 
@@ -29,8 +38,11 @@ class DequeSet:
             self._set.remove(discared)
 
 
-class Adjudicator(BlueskyConsumer, ABC):
-    """An agent adjudicator that listens to published suggestions by agents."""
+class AdjudicatorBase(BlueskyConsumer, ABC):
+    """
+    An agent adjudicator that listens to published suggestions by agents.
+    This Base approach (as per `process_document`) only retains the most recent suggestions by any named agents.
+    """
 
     def __init__(self, *args, tla: str, switchboard_prefix=None, **kwargs):
         super().__init__(*args, **kwargs)
@@ -39,7 +51,10 @@ class Adjudicator(BlueskyConsumer, ABC):
         self._tla = tla.lower()
         switchboard_prefix = switchboard_prefix if switchboard_prefix is not None else f"{tla}:switchboard:"
         self._switchboard = SwitchBoardBackend(switchboard_prefix, name="switchboard")
-        self._current_suggestions = {}  # agent_name: doc
+        self._current_suggestions = {}  # agent_name: AdjudicatorMsg
+        self.re_manager = REManagerAPI(http_server_uri=self.server_host)
+        self.re_manager.set_authorization_key(api_key=self.api_key)
+        self._uid_deque_set = DequeSet()
 
     def start(self, *args, **kwargs):
         self._callback_id = self._switchboard.publish_to_queue.subscribe(self.switchboard_callback)
@@ -47,12 +62,6 @@ class Adjudicator(BlueskyConsumer, ABC):
             super().start(*args, **kwargs)
         finally:
             self._switchboard.publish_to_queue.unsubscribe(self._callback_id)
-
-    def spin(self):
-        if self._thread is not None:
-            raise RuntimeError("Adjudicator has already started spinning, this can't be done twice.")
-        self._thread = Thread(target=self.start)
-        self._thread.start()
 
     def switchboard_callback(self, value):
         """Process to run when switch board signal is set to on."""
@@ -70,15 +79,78 @@ class Adjudicator(BlueskyConsumer, ABC):
             return True
         with self._lock:
             logger.info(f"{doc['agent_name']=}, {doc['suggestions_uid']=}")
-            self._current_suggestions[doc["agent_name"]] = doc
+            self._current_suggestions[doc["agent_name"]] = AdjudicatorMsg(**doc)
 
     @property
     def current_suggestions(self):
+        """Dictionary of {agent_name:AdjudicatorMsg}, deep copied at each grasp."""
         with self._lock:
-            ret = dict(self._current_suggestions)
+            ret = deepcopy(self._current_suggestions)
+        return ret
+
+    @property
+    def agent_names(self):
+        with self._lock:
+            ret = list(self._current_suggestions.keys())
         return ret
 
     @abstractmethod
     def make_judgements(self, value):
         """How to make decisions when given the call of duty"""
         ...
+
+    @property
+    @abstractmethod
+    def server_host(self):
+        """
+        Host to POST requests to. Declare as property or as class level attribute.
+        Something akin to 'http://localhost:60610'
+        """
+        ...
+
+    @property
+    @abstractmethod
+    def api_key(self):
+        """
+        Key for API security.
+        """
+        ...
+
+    def _add_suggestion_to_queue(self, agent_name, suggestion):
+        kwargs = suggestion.plan_kwargs
+        kwargs.setdefault("md", {})
+        kwargs["md"]["agent_ask_uid"] = suggestion.ask_uid
+        kwargs["md"]["agent_name"] = agent_name
+        plan = BPlan(suggestion.plan_name, *suggestion.plan_args, **kwargs)
+        r = self.re_manager.item_add(plan, pos="back")
+        logging.debug(f"Sent http-server request by adjudicator\n." f"Received reponse: {r}")
+
+    def add_current_suggestions_to_queue(self, agent_name):
+        """Adds all current suggestions by an agent to the queue"""
+
+
+class AgentByModeAdjudicator(AdjudicatorBase):
+    def make_judgements(self, value):
+        agent_name = self._switchboard.adjudicate_mode.get()
+        if agent_name not in self.agent_names:
+            logger.warning(f"Agent {agent_name} not known to the Adjudicator")
+        else:
+            adjudicator_msg = self.current_suggestions[agent_name]
+            for suggestion in adjudicator_msg.suggestions[self._tla]:
+                self._add_suggestion_to_queue(agent_name, suggestion)
+
+
+if __name__ == "__main__":
+    import uuid
+
+    kafka_config = _read_bluesky_kafka_config_file(config_file_path="/etc/bluesky/kafka.yml")
+    tla = "pdf"
+
+    adjudicator = AgentByModeAdjudicator(
+        tla=tla,
+        topics=[f"{tla}.mmm.bluesky.adjudicators"],
+        bootstrap_servers=",".join(kafka_config["bootstrap_servers"]),
+        group_id=f"echo-{tla}-{str(uuid.uuid4())[:8]}",
+        consumer_config=kafka_config["runengine_producer_config"],
+    )
+    adjudicator.start()
