be482c749cce3ddb9be25b34737746446e0ae03c
maffettone <phillip.maffettone@gmail.com>
2022-11-08 21:29:57 -0500

    temporarily add ask for bmm

--- mmm_experiments/scripts/run_bmm_passive_sva.py
+++ mmm_experiments/scripts/run_bmm_passive_sva.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
             origin=(183.818, 122.319),
             relative_bounds=(-28, 37),
             metadata=dict(init_time=time.time(), notes="Agent run on half wafer as initial test"),
-            ask_on_tell=False,
+            ask_on_tell=True,
+            direct_to_queue=False,
             report_on_tell=True,
         )
         signal.signal(signal.SIGINT, agent.signal_handler)
